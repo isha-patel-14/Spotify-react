b9d4e622d0f6cde73f612ff08079bf513dc8f41a
isha-patel-14 <isha.patel.cg@gmail.com>
2024-12-05 10:14:58 +0530

    sidebar and navbar added

--- spotify_react/src/App.jsx
+++ spotify_react/src/App.jsx
@@ -1,11 +1,57 @@
 import './App.css'
-
+import logo from './assets/Spotify logo.png'
+import home from './assets/Home (2).png' 
+import search from './assets/Search (2).png'
+import lib from './assets/Your library.png'
+import create from './assets/Create (2).png'
+import like from './assets/Liked (2).png'
+import episode from './assets/your episodes.png'
+import icon from './assets/grommet-icons_install-option.png'
+import topArrow from './assets/top arrow navigation.png'
+import user from './assets/User.png'
 
 function App() {
 
   return (
     <>
-      
+      <div className="sidebar">
+        <div className="home">
+            <div className="logo">
+                <img src={logo} className='logo_spotify'/>
+            </div>
+            <div className="home1">
+                <img src={home} className="h_img"/>
+                Home
+            </div>
+            <div><img  src={search} className="h_img"/>Search</div>
+            <div><img  src={lib} className="h_img"/>Your Library</div>
+        </div>
+
+        <div className="playlist">
+            <div><img src={create} className="h_img"/>Create Playlist</div>
+            <div><img src={like} className="h_img" />Liked Songs</div>
+            <div><img src={episode} className="h_img" />Your Episodes</div>
+        </div>
+
+        <div className="fav">
+            <div className="FAV">FAV</div>
+            <div className="f_img">DailyMix1</div>
+            <div className="f_img">DiscoverWeekly</div>
+            <div className="f_img">Malyalam</div>
+            <div className="f_img">Dance/ElectronicMix</div>
+            <div className="f_img">EDM/Popular</div>
+        </div>
+
+        <div className="install">
+            <div><img src={icon} className="i_img" />Install App</div>
+        </div>
+    </div>
+
+    <div class="nav">
+        <div><img src={topArrow} class="nav-i1"/></div>
+        <div><img src={user} class="nav-i2"/></div>
+    </div>
+
     </>
   )
 }
